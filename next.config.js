928639d1cbe32b65ba8a5a5d973dee3c5964e375
Bryan Altman <altmbr@gmail.com>
2025-11-18 19:19:02 -0500

    Force redeployment to clear cache

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Force cache clear - deployment trigger
+  // Force cache clear - deployment trigger v2
   images: {
     // Enable Next.js image optimization for better Core Web Vitals
     remotePatterns: [
