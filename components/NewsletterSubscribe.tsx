4d33c07c10acc2cc6cf9fb92ac3ef2201d3e617a
Bryan Altman <altmbr@gmail.com>
2025-11-24 23:22:23 -0500

    Change footer text to Email Podcast Summaries

--- components/NewsletterSubscribe.tsx
+++ components/NewsletterSubscribe.tsx
@@ -73,7 +73,7 @@ export default function NewsletterSubscribe() {
                 fontWeight: 'var(--font-weight-medium)'
               }}
             >
-              Weekly Summary
+              Email Podcast Summaries
             </p>
             <p
               style={{
